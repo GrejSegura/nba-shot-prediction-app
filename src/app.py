0e5b3d9b1cf8129652bac45c996e646363f6df1b
mr_grej <mr_grej@yahoo.com>
2020-08-18 07:34:45 +0400

    Update app.py

--- src/app.py
+++ src/app.py
@@ -9,6 +9,7 @@ import torch
 import torch.nn.functional as F
 import torch.utils.data
 import torch.optim as optim
+from models import *
 
 from PIL import Image
 
@@ -128,53 +129,6 @@ def data_process(df):
     return data
 
 processed_data = data_process(df)
-
-# LOAD DNN (PYTORCH) MODEL ARCHITECTURE
-class Classifier(nn.Module):
-    def __init__(self):
-        super().__init__()
-        
-        self.fc1 = nn.Linear(len(column_list), 200)
-        self.relu1 = nn.ReLU()
-        self.dout = nn.Dropout(0.3)
-                
-        self.fc2 = nn.Linear(200, 200)
-        self.prelu = nn.PReLU(1)
-        self.dout = nn.Dropout(0.3)
-        
-        self.fc6 = nn.Linear(200, 100)
-        self.prelu = nn.PReLU(1)
-        
-        self.out = nn.Linear(100, 1)
-        self.out_act = nn.Sigmoid()
-        
-    def forward(self, input_):
-        a1 = self.fc1(input_)
-        h1 = self.relu1(a1)
-        dout = self.dout(h1)
-        
-        a2 = self.fc2(dout)
-        h2 = self.prelu(a2)
-        dout = self.dout(h2)
-        
-        a6 = self.fc6(dout)
-        h6 = self.prelu(a6)
-        
-        a7 = self.out(h6)
-        
-        y = self.out_act(a7)
-        return y
-
-# PREDICT THE OUTCOME
-def predict_pytorch(data):
-    dtype = torch.FloatTensor
-
-    data_1 = torch.tensor(data.values).type(dtype)
-
-    with torch.no_grad():
-        y_pred = model.forward(data_1).cpu().numpy()
-    
-    return y_pred
 
 model = Classifier()
 model.load_state_dict(torch.load(r'./models/model_pytorch.pt'))
